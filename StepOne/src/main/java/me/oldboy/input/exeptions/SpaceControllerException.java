23035d38041723fd998a3c6c896e6cec3d9de670
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-07 20:38:06 +0500

    StepOne: Add SpaceControllerException.java

---
+++ StepOne/src/main/java/me/oldboy/input/exeptions/SpaceControllerException.java
@@ -0,0 +1,7 @@
+package me.oldboy.input.exeptions;
+
+public class SpaceControllerException extends RuntimeException {
+    public SpaceControllerException(String message) {
+        super(message);
+    }
+}
