e42940cfd26bee6afebeaa5da84c0899f95e6734
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-07 20:37:54 +0500

    StepOne: Add ReserveBaseException.java

---
+++ StepOne/src/main/java/me/oldboy/input/exeptions/ReserveBaseException.java
@@ -0,0 +1,7 @@
+package me.oldboy.input.exeptions;
+
+public class ReserveBaseException extends RuntimeException {
+    public ReserveBaseException(String message) {
+        super(message);
+    }
+}
