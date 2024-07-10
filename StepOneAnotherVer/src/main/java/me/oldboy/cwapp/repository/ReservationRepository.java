c8159f5bfd23aa99149e6a4d8eba4152d4ed5f9a
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-10 20:51:41 +0500

    StepOneAnotherVer: Add ReservationRepository.java

---
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/repository/ReservationRepository.java
@@ -0,0 +1,20 @@
+package me.oldboy.cwapp.repository;
+
+import me.oldboy.cwapp.entity.Place;
+import me.oldboy.cwapp.entity.Reservation;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface ReservationRepository {
+    Long create(Reservation newReservation);
+    Reservation update(Reservation reservation);
+    Optional<Reservation> findById(Long reservationId);
+    boolean delete(Long reservationId);
+    Optional<List<Reservation>> findByDate(LocalDate reservationDate);
+    Optional<List<Reservation>> findByUserId(Long userId);
+    Optional<List<Reservation>> findByPlaceId(Long placeId);
+    Optional<List<Reservation>> findByDateAndPlace(LocalDate reservationDate, Long reservationPlaceId);
+    List<Reservation> findAll();
+}
