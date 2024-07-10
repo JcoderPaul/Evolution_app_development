d5bae208d28fd0fbf0c56c8d2648bdb99e4f40f6
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-10 20:49:39 +0500

    StepOneAnotherVer: Add User.java

---
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/entity/User.java
@@ -0,0 +1,20 @@
+package me.oldboy.cwapp.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long userId;
+    private String userLogin;
+    private String passWord;
+    private Role role;
+
+    public User(String userLogin, String passWord, Role role) {
+        this.userLogin = userLogin;
+        this.passWord = passWord;
+        this.role = role;
+    }
+}
