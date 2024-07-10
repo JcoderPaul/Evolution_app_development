088da10f5d84ef10ef8e1d0b1ad4684fa5933334
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-10 20:51:55 +0500

    StepOneAnotherVer: Add UserRepository.java

---
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package me.oldboy.cwapp.repository;
+
+import me.oldboy.cwapp.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+    Long create(User user);
+    Optional<User> findById(Long userId);
+    User update(User user);
+    boolean delete(Long userId);
+    Optional<User> findUserByLogin(String userLogin);
+    List<User> findAll();
+
+}
