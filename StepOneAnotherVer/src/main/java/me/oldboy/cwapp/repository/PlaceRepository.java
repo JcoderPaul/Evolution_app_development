c761114e2289626594a0d0316314d22070071f41
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-10 20:51:28 +0500

    StepOneAnotherVer: Add PlaceRepository.java

---
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/repository/PlaceRepository.java
@@ -0,0 +1,17 @@
+package me.oldboy.cwapp.repository;
+
+import me.oldboy.cwapp.entity.Place;
+import me.oldboy.cwapp.entity.Species;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlaceRepository {
+    Long create(Place place);
+    Place update(Place place);
+    Optional<Place> findById(Long placeId);
+    boolean delete(Long placeId);
+    Optional<Place> findBySpeciesAndPlaceNumber(Species species, Integer placeNumber);
+    List<Place> findAll();
+    List<Place> findAllBySpecies(Species species);
+}
