c928d1293dca3e8cbb2121b5f51439b5b7754e76
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-10 20:48:45 +0500

    StepOneAnotherVer: Add Place.java

---
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/entity/Place.java
@@ -0,0 +1,21 @@
+package me.oldboy.cwapp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Place {
+    private Long placeId;
+    private Species species;
+    private Integer placeNumber;
+
+    public Place(Species species, Integer placeNumber) {
+        this.species = species;
+        this.placeNumber = placeNumber;
+    }
+}
