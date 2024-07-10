b57064086ec9083cb498f56248f41210f0fbdbe6
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-10 20:50:37 +0500

    StepOneAnotherVer: Add ReservationBaseException.java

---
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/exception/ReservationBaseException.java
@@ -0,0 +1,7 @@
+package me.oldboy.cwapp.exception;
+
+public class ReservationBaseException extends RuntimeException {
+    public ReservationBaseException(String msg) {
+        super(msg);
+    }
+}
