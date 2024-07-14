9816f322ba7312c34ceccfc6fad7370ef04ac067
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-14 09:07:57 +0500

    StepOneAnotherVer: Edit ReservationServiceException.java

---
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/exception/service_exception/ReservationServiceException.java
@@ -0,0 +1,7 @@
+package me.oldboy.cwapp.exception.service_exception;
+
+public class ReservationServiceException extends RuntimeException {
+    public ReservationServiceException(String msg) {
+        super(msg);
+    }
+}
