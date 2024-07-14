33d7127c830516ed98279b5df6c61a49e8f102ac
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-14 09:07:36 +0500

    StepOneAnotherVer: Edit PlaceServiceException.java

---
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/exception/service_exception/PlaceServiceException.java
@@ -0,0 +1,7 @@
+package me.oldboy.cwapp.exception.service_exception;
+
+public class PlaceServiceException extends RuntimeException {
+    public PlaceServiceException(String msg) {
+        super(msg);
+    }
+}
