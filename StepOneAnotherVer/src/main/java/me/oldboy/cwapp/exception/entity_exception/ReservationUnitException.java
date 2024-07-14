63362348876d41d0ee3d27f8ab119eec32503e5f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-14 09:07:25 +0500

    StepOneAnotherVer: Edit ReservationUnitException.java

--- StepOneAnotherVer/src/main/java/me/oldboy/cwapp/exception/entity_exception/ReservationUnitException.java
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/exception/entity_exception/ReservationUnitException.java
@@ -1,4 +1,4 @@
-package me.oldboy.cwapp.exception;
+package me.oldboy.cwapp.exception.entity_exception;
 
 public class ReservationUnitException extends IllegalArgumentException {
     public ReservationUnitException(String msg) {
