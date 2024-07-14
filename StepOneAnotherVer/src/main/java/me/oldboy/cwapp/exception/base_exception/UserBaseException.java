3cf6cd99f77a6eb322637b7e1a742c216f602751
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-14 09:07:14 +0500

    StepOneAnotherVer: Edit UserBaseException.java

--- StepOneAnotherVer/src/main/java/me/oldboy/cwapp/exception/base_exception/UserBaseException.java
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/exception/base_exception/UserBaseException.java
@@ -1,4 +1,4 @@
-package me.oldboy.cwapp.exception;
+package me.oldboy.cwapp.exception.base_exception;
 
 public class UserBaseException extends RuntimeException {
     public UserBaseException(String msg) {
