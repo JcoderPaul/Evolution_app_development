5f414646aa7cc27ee5e5fb8494a4bd886513957d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-19 17:45:23 +0500

    StepOneAnotherVer: Add ReservationHandlerException.java

---
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/exception/handlers_exception/ReservationHandlerException.java
@@ -0,0 +1,7 @@
+package me.oldboy.cwapp.exception.handlers_exception;
+
+public class ReservationHandlerException extends IllegalArgumentException {
+    public ReservationHandlerException(String msg) {
+        super(msg);
+    }
+}
