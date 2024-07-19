e21a53f8d5cce2149d1d2a0e15ee936ae6810592
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-19 17:45:38 +0500

    StepOneAnotherVer: Add UserAuthenticationHandlerException.java

---
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/exception/handlers_exception/UserAuthenticationHandlerException.java
@@ -0,0 +1,7 @@
+package me.oldboy.cwapp.exception.handlers_exception;
+
+public class UserAuthenticationHandlerException extends IllegalArgumentException {
+    public UserAuthenticationHandlerException(String msg) {
+        super(msg);
+    }
+}
