d1c38d99b11844d090aeef80db7c30694aeb1b7c
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-19 17:45:04 +0500

    StepOneAnotherVer: Add PlaceViewHandlerException.java

---
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/exception/handlers_exception/PlaceViewHandlerException.java
@@ -0,0 +1,7 @@
+package me.oldboy.cwapp.exception.handlers_exception;
+
+public class PlaceViewHandlerException extends RuntimeException {
+    public PlaceViewHandlerException(String msg) {
+        super(msg);
+    }
+}
