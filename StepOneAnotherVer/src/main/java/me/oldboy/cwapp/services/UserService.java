79051344f78aeb9d54c996453cd4bff3acbd4634
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-19 17:53:24 +0500

    StepOneAnotherVer: Edit UserService.java

--- StepOneAnotherVer/src/main/java/me/oldboy/cwapp/services/UserService.java
+++ StepOneAnotherVer/src/main/java/me/oldboy/cwapp/services/UserService.java
@@ -1,37 +1,44 @@
 package me.oldboy.cwapp.services;
 
+import lombok.RequiredArgsConstructor;
 import me.oldboy.cwapp.store.base.UserBase;
 import me.oldboy.cwapp.entity.User;
 import me.oldboy.cwapp.exception.service_exception.UserServiceException;
+import me.oldboy.cwapp.store.repository.UserRepository;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 public class UserService {
 
-    private UserBase userBase;
-
-    private UserService(UserBase userBase) {
-        this.userBase = userBase;
-    }
+    private final UserRepository userRepository;
 
     public Long registration(User user){
         Long regId = null;
-        if ((userBase.findUserByLogin(user.getUserLogin()).isEmpty()) && (user.getUserId() == null)){
-            regId = userBase.create(user);
+        if ((userRepository.findUserByLogin(user.getUserLogin()).isEmpty()) && (user.getUserId() == null)){
+            regId = userRepository.create(user);
         } else {
-            throw new UserServiceException("Логин: " + user.getUserLogin() + "уже есть в системе или " +
+            throw new UserServiceException("Логин: " + user.getUserLogin() + " уже есть в системе или " +
                                            "переданные данные содержат недопустимые значения, например ID!");
         }
         return regId;
     }
 
     public User login(String login, String passWord){
-        Optional<User> loggedInUser = userBase.findUserByLogin(login);
+        Optional<User> loggedInUser = userRepository.findUserByLogin(login);
         if(loggedInUser.isEmpty()) {
             throw new UserServiceException("Пользователь не зарегистрирован!");
         } else if (!loggedInUser.get().getPassWord().equals(passWord)) {
             throw new UserServiceException("Неверный пароль!");
         } else
             return loggedInUser.get();
+    }
+
+    public User getExistUserById(Long userId){
+        Optional<User> mayBeUser = userRepository.findById(userId);
+        if(mayBeUser.isEmpty()){
+            throw new UserServiceException("Пользователь c ID: " + userId + " не найден!");
+        } else
+            return mayBeUser.get();
     }
 }
