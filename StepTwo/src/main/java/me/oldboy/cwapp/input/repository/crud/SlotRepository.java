a3cd19253a559cdffd172dab1dac5fe5d0749e04
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-27 21:27:17 +0500

    StepTwo: Add SlotRepository.java

---
+++ StepTwo/src/main/java/me/oldboy/cwapp/input/repository/crud/SlotRepository.java
@@ -0,0 +1,20 @@
+package me.oldboy.cwapp.input.repository.crud;
+
+import me.oldboy.cwapp.input.entity.Slot;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SlotRepository {
+    /* CRUD - Create */
+    Optional<Slot> createSlot(Slot slot);
+    /* CRUD - Read */
+    List<Slot> findAllSlots();
+    Optional<Slot> findSlotById(Long placeId);
+
+    Optional<Slot> findSlotByNumber(Integer slotNumber);
+    /* CRUD - Update */
+    boolean updateSlot(Slot slot);
+    /* CRUD - Delete */
+    boolean deleteSlot(Long slotId);
+}
