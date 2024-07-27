94e4a0946959780e7a249fec72adcac10f1af8f0
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-27 21:27:04 +0500

    StepTwo: Add ReservationRepository.java

---
+++ StepTwo/src/main/java/me/oldboy/cwapp/input/repository/crud/ReservationRepository.java
@@ -0,0 +1,26 @@
+package me.oldboy.cwapp.input.repository.crud;
+
+import me.oldboy.cwapp.input.entity.Reservation;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface ReservationRepository {
+    /* CRUD - Create */
+    Optional<Reservation> createReservation(Reservation newReservation);
+    /* CRUD - Read */
+    Optional<Reservation> findReservationById(Long reservationId);
+    Optional<List<Reservation>> findAllReservation();
+    Optional<List<Reservation>> findReservationByDate(LocalDate reservationDate);
+    Optional<List<Reservation>> findReservationByUserId(Long userId);
+    Optional<List<Reservation>> findReservationByPlaceId(Long placeId);
+    Optional<List<Reservation>> findReservationBySlotId(Long slotId);
+    Optional<Reservation> findReservationByDatePlaceAndSlot(LocalDate reservationDate,
+                                                            Long placeId,
+                                                            Long slotId);
+    /* CRUD - Update */
+    boolean updateReservation(Reservation reservation);
+    /* CRUD - Delete */
+    boolean deleteReservation(Long reservationId);
+}
