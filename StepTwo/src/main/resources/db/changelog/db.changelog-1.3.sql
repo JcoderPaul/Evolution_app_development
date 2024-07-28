f1a99166177cc0a1cea94c3a73bddd50ffe01763
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-07-28 20:38:50 +0500

    StepTwo: Add db.changelog-1.3.sql

---
+++ StepTwo/src/main/resources/db/changelog/db.changelog-1.3.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+
+--changeset oldboy:1
+CREATE TABLE IF NOT EXISTS coworking.all_reserves
+(
+    reserve_id BIGSERIAL PRIMARY KEY ,
+    reserve_date DATE NOT NULL CONSTRAINT right_data CHECK (reserve_date >= NOW()) ,
+    user_id BIGINT REFERENCES coworking.users (user_id) ,
+    place_id BIGINT REFERENCES coworking.places (place_id) ,
+    slot_id BIGINT REFERENCES coworking.slots (slot_id) ,
+    UNIQUE (reserve_date, user_id, place_id, slot_id)
+);
+--rollback drop table coworking.all_reserves;
