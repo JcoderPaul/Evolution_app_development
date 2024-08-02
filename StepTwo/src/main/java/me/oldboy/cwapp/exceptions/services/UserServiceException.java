8bbfc929f21056b153fcd1c51b35536efa7a5e5b
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-08-02 08:45:50 +0500

    StepTwo: Add UserServiceException.java

---
+++ StepTwo/src/main/java/me/oldboy/cwapp/exceptions/services/UserServiceException.java
@@ -0,0 +1,6 @@
+package me.oldboy.cwapp.exceptions.services;
+
+public class UserServiceException extends RuntimeException {
+    public UserServiceException(String message) {super(message);
+    }
+}
