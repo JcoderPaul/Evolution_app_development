2108c5c2b13ddbd7afc1b5d3ffe948cabc93fcb4
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-08-02 22:19:01 +0500

    StepTwo: Add ReserveControllerException.java

---
+++ StepTwo/src/main/java/me/oldboy/cwapp/exceptions/controllers/ReserveControllerException.java
@@ -0,0 +1,7 @@
+package me.oldboy.cwapp.exceptions.controllers;
+
+public class ReserveControllerException extends RuntimeException {
+    public ReserveControllerException(String msg) {
+        super(msg);
+    }
+}
