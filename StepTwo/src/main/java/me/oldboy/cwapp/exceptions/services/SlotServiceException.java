19653f0a22f0e721099eb92e99b589ec4cb90ddd
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-08-02 08:45:39 +0500

    StepTwo: Add SlotServiceException.java

---
+++ StepTwo/src/main/java/me/oldboy/cwapp/exceptions/services/SlotServiceException.java
@@ -0,0 +1,7 @@
+package me.oldboy.cwapp.exceptions.services;
+
+public class SlotServiceException extends RuntimeException {
+    public SlotServiceException(String msg) {
+        super(msg);
+    }
+}
