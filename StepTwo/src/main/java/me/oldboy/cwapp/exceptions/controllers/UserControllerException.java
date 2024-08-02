2e0acd6f97a54b10d0d1f1c4e605df7772372b69
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-08-02 08:48:08 +0500

    StepTwo: Add UserControllerException.java

---
+++ StepTwo/src/main/java/me/oldboy/cwapp/exceptions/controllers/UserControllerException.java
@@ -0,0 +1,7 @@
+package me.oldboy.cwapp.exceptions.controllers;
+
+public class UserControllerException extends RuntimeException {
+    public UserControllerException(String msg) {
+        super(msg);
+    }
+}
