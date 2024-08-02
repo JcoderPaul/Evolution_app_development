19d0c49bd18b0bdedc66932382afbd6a70a96b0f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-08-02 08:45:27 +0500

    StepTwo: Add ReserveServiceException.java

---
+++ StepTwo/src/main/java/me/oldboy/cwapp/exceptions/services/ReserveServiceException.java
@@ -0,0 +1,7 @@
+package me.oldboy.cwapp.exceptions.services;
+
+public class ReserveServiceException extends RuntimeException {
+    public ReserveServiceException(String msg) {
+        super(msg);
+    }
+}
