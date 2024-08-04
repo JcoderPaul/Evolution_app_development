bc9e4f3720d18a3e74a8d2c4fd18a164438c34c8
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-08-04 08:09:31 +0500

    StepTwo: Add UserRepositoryImpl.java

--- StepTwo/src/main/java/me/oldboy/cwapp/input/repository/UserRepositoryImpl.java
+++ StepTwo/src/main/java/me/oldboy/cwapp/input/repository/UserRepositoryImpl.java
@@ -1,7 +1,6 @@
 package me.oldboy.cwapp.input.repository;
 
 import lombok.RequiredArgsConstructor;
-import me.oldboy.cwapp.exceptions.repositorys.UserRepositoryException;
 import me.oldboy.cwapp.input.entity.Role;
 import me.oldboy.cwapp.input.entity.User;
 import me.oldboy.cwapp.input.repository.crud.UserRepository;
@@ -88,6 +87,7 @@ public class UserRepositoryImpl implements UserRepository {
     /* Блок работы с сущностью user */
     @Override
     public Optional<User> createUser(User user) {
+        User userToBase = null;
         /*
         Для нас важно получить сгенерированный базой ID записи, поэтому мы передаем константу,
         которая указывает, что сгенерированные ключи должны быть доступны для извлечения. Акцент
@@ -107,87 +107,84 @@ public class UserRepositoryImpl implements UserRepository {
             */
             prepareStatement.executeUpdate();
             ResultSet generatedAutoId = prepareStatement.getGeneratedKeys();
-            User userToBase = null;
+
             if(generatedAutoId.next())
             {
                 long id = generatedAutoId.getLong("user_id");
                 userToBase = new User(id, user.getLogin(), user.getPassword());
             }
-            return Optional.ofNullable(userToBase);
+
         } catch (SQLException sqlException) {
-            throw new UserRepositoryException(sqlException);
+            sqlException.printStackTrace();
         }
+        return Optional.ofNullable(userToBase);
     }
 
     @Override
     public List<User> findAllUsers() {
+        List<User> findAllUsers = new ArrayList<>();
         try(PreparedStatement prepareStatement = connection.prepareStatement(FIND_ALL_USERS_SQL)) {
-
             ResultSet resultOfQuery = prepareStatement.executeQuery();
-            List<User> findAllUsers = new ArrayList<>();
             while (resultOfQuery.next()){
                 findAllUsers.add(buildUser(resultOfQuery));
             }
-            return findAllUsers;
+
         } catch (SQLException sqlException) {
-            throw new UserRepositoryException(sqlException);
+            sqlException.printStackTrace();
         }
+        return findAllUsers;
     }
 
     @Override
     public Optional<User> findUserById(Long id) {
+        User mayBeUser = null;
         try (PreparedStatement prepareStatement = connection.prepareStatement(FIND_USER_BY_ID_SQL)) {
-
             prepareStatement.setLong(1, id);
-
             ResultSet resultSet = prepareStatement.executeQuery();
-            User mayBeUser = null;
             if (resultSet.next()) {
                 mayBeUser = buildUser(resultSet);
             }
-            return Optional.ofNullable(mayBeUser);
+
         } catch (SQLException sqlException) {
-            throw new UserRepositoryException(sqlException);
+            sqlException.printStackTrace();
         }
+        return Optional.ofNullable(mayBeUser);
     }
 
     @Override
     public Optional<User> findUserByLogin(String login) {
+        User mayBeUser = null;
         try (PreparedStatement prepareStatement = connection.prepareStatement(FIND_USER_BY_LOGIN_SQL)) {
-
             prepareStatement.setString(1, login);
-
             ResultSet resultSet = prepareStatement.executeQuery();
-            User mayBeUser = null;
             if (resultSet.next()) {
                 mayBeUser = buildUser(resultSet);
             }
-            return Optional.ofNullable(mayBeUser);
         } catch (SQLException sqlException) {
-            throw new UserRepositoryException(sqlException);
+            sqlException.printStackTrace();
         }
+        return Optional.ofNullable(mayBeUser);
     }
 
     @Override
     public Optional<User> findUserByLoginAndPassword(String login, String password) {
+        User mayBeUser = null;
         try (PreparedStatement prepareStatement = connection.prepareStatement(FIND_USER_BY_LOGIN_AND_PASSWORD_SQL)) {
-
             prepareStatement.setString(1, login);
             prepareStatement.setString(2, password);
-
             ResultSet resultSet = prepareStatement.executeQuery();
-            User mayBeUser = null;
             if (resultSet.next()) {
                 mayBeUser = buildUser(resultSet);
             }
-            return Optional.ofNullable(mayBeUser);
         } catch (SQLException sqlException) {
-            throw new UserRepositoryException(sqlException);
+            sqlException.printStackTrace();
         }
+        return Optional.ofNullable(mayBeUser);
     }
 
     @Override
     public boolean updateUser(User user) {
+        Boolean isUserUpdated = false;
         try(PreparedStatement prepareStatement = connection.prepareStatement(UPDATE_USER_SQL)) {
 
             prepareStatement.setString(1, user.getLogin());
@@ -195,22 +192,23 @@ public class UserRepositoryImpl implements UserRepository {
             prepareStatement.setString(3, user.getRole().name());
             prepareStatement.setLong(4, user.getUserId());
 
-            return prepareStatement.executeUpdate() > 0;
+            isUserUpdated = prepareStatement.executeUpdate() > 0;
         } catch (SQLException sqlException) {
-            throw new UserRepositoryException(sqlException);
+            sqlException.printStackTrace();
         }
+        return isUserUpdated;
     }
 
     @Override
     public boolean deleteUser(Long userId) {
+        Boolean isUserDeleted = false;
         try(PreparedStatement prepareStatement = connection.prepareStatement(DELETE_USER_BY_ID_SQL)) {
-
             prepareStatement.setLong(1, userId);
-
-            return prepareStatement.executeUpdate() > 0;
+            isUserDeleted = prepareStatement.executeUpdate() > 0;
         } catch (SQLException sqlException) {
-            throw new UserRepositoryException(sqlException);
+            sqlException.printStackTrace();
         }
+        return isUserDeleted;
     }
 
     private User buildUser(ResultSet resultSet) throws SQLException {
