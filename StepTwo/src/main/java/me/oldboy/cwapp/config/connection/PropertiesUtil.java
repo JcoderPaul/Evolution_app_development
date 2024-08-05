f5e8226cb6104147a267e0ab1216f1a14c6ca1af
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-08-05 22:08:57 +0500

    StepTwo: Add PropertiesUtil.java

---
+++ StepTwo/src/main/java/me/oldboy/cwapp/config/connection/PropertiesUtil.java
@@ -0,0 +1,35 @@
+package me.oldboy.cwapp.config.connection;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Класс предназначенный для чтения "application.properties" файла.
+ *
+ * Singleton utility class for reading properties from the "application.properties" file.
+ */
+public class PropertiesUtil {
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        loadProperties();
+    }
+
+    private PropertiesUtil() {
+    }
+
+    public static String get(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+
+    private static void loadProperties() {
+        try (InputStream inputStream =
+                     PropertiesUtil.class.getClassLoader().getResourceAsStream("application.properties"))
+        {
+            PROPERTIES.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
