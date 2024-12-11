d18ef2174455004007bd014c4e1863b46175b8c8
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-12-11 06:42:16 +0500

    StepThree: Add AuditOperationType.java

---
+++ StepThree/src/main/java/me/oldboy/core/model/database/audit/operations/AuditOperationType.java
@@ -0,0 +1,19 @@
+package me.oldboy.core.model.database.audit.operations;
+
+/**
+ * Enumeration representing different operation with entity (Place, Slot, User, Reservation).
+ * Список наиболее возможных операций с сущностями приложения.
+ */
+public enum AuditOperationType {
+    CREATE_RESERVATION,
+    DELETE_RESERVATION,
+    UPDATE_RESERVATION,
+    CREATE_SLOT,
+    DELETE_SLOT,
+    UPDATE_SLOT,
+    CREATE_PLACE,
+    DELETE_PLACE,
+    UPDATE_PLACE,
+    DELETE_USER,
+    UPDATE_USER
+}
