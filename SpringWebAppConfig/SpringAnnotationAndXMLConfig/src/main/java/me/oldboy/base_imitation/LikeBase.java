a482460d76bf1145bdd9dd38a5a7e418dde266a7
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-01-14 16:23:03 +0500

    SpringAnnotationAndXMLConfig: add LikeBase.java

---
+++ SpringWebAppConfig/SpringAnnotationAndXMLConfig/src/main/java/me/oldboy/base_imitation/LikeBase.java
@@ -0,0 +1,21 @@
+package me.oldboy.base_imitation;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import me.oldboy.like_entity.User;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class LikeBase {
+    private List<User> likeBase = List.of(new User(1L,"John"),
+                                          new User(2L,"Will"),
+                                          new User(3L,"Sally"));
+
+    public List<User> getLikeBase() {
+        return likeBase;
+    }
+}
