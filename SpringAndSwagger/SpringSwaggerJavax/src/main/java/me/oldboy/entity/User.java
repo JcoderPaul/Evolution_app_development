78f360e0d3877ecc1e662bcd1450f026695043b8
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-01-31 12:03:48 +0500

    SpringSwaggerJavax: add User.java

---
+++ SpringAndSwagger/SpringSwaggerJavax/src/main/java/me/oldboy/entity/User.java
@@ -0,0 +1,31 @@
+package me.oldboy.entity;
+
+import lombok.*;
+import me.oldboy.entity.options.Role;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "users", schema = "coworking")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "login")
+    private String userName;
+
+    @Column(name = "user_pass")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+}
