91684f6b88e6c4658c9625d79dd493c618ef9ac8
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-13 00:27:13 +0500

    StepFour: add PlaceRepository.java

---
+++ StepFour/src/main/java/me/oldboy/repository/PlaceRepository.java
@@ -0,0 +1,29 @@
+package me.oldboy.repository;
+
+import me.oldboy.models.entity.Place;
+import me.oldboy.models.entity.options.Species;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+    @Query(value = "SELECT p.* " +
+            "FROM coworking.places AS p " +
+            "WHERE (p.species =:species " +
+            "AND p.place_number =:number)",
+            nativeQuery = true)
+    public Optional<Place> findBySpeciesAndNumber(@Param("species") String species,
+                                                  @Param("number") Integer number);
+
+    @Query(value = "SELECT p.* " +
+            "FROM coworking.places AS p " +
+            "WHERE p.species =:species",
+            nativeQuery = true)
+    public Optional<List<Place>> findAllBySpecies(@Param("species") String species);
+}
