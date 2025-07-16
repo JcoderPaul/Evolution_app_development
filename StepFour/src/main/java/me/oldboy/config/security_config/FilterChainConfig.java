27f026c8482d9d18963d2a71229a971036e0f402
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-16 06:59:20 +0500

    StepFour: add FilterChainConfig.java

---
+++ StepFour/src/main/java/me/oldboy/config/security_config/FilterChainConfig.java
@@ -0,0 +1,25 @@
+package me.oldboy.config.security_config;
+
+import me.oldboy.config.jwt_config.JwtAuthFilter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+public class FilterChainConfig {
+    public static SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity, JwtAuthFilter jwtAuthFilter) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(urlConfig -> urlConfig
+                        .requestMatchers("/api/registration", "/api/login").permitAll()
+                        .requestMatchers("/api/places/**", "/api/slots/**", "/api/reservations/**").authenticated()
+                        .requestMatchers("/api/admin/**").hasAuthority("ADMIN")
+                        .anyRequest().authenticated());
+
+        return httpSecurity.build();
+    }
+}
