9a78a543bfc1bb7f5a9e0a6b1df6093a81106c0d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-16 06:58:45 +0500

    StepFour: add MainAppConfig.java

---
+++ StepFour/src/main/java/me/oldboy/config/main_config/MainAppConfig.java
@@ -0,0 +1,21 @@
+package me.oldboy.config.main_config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+        "me.oldboy.config.data_source"
+        ,"me.oldboy.config.security_config"
+        ,"me.oldboy.config.security_details"
+        ,"me.oldboy.config.jwt_config"
+        ,"me.oldboy.controllers"
+        ,"me.oldboy.services"
+        ,"me.oldboy.repository"
+        ,"me.oldboy.exception"
+})
+public class MainAppConfig {
+
+}
