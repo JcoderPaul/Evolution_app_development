4507979be6901ac0ce2297209dcf9d4ac7c1a8e7
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-18 07:45:47 +0500

    StepFour: add JwtAuthResponse.java

---
+++ StepFour/src/main/java/me/oldboy/dto/jwt/JwtAuthResponse.java
@@ -0,0 +1,30 @@
+package me.oldboy.dto.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtAuthResponse {
+
+    private Long id;
+    private String login;
+    private String accessToken;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JwtAuthResponse that = (JwtAuthResponse) o;
+        return Objects.equals(id, that.id) && Objects.equals(login, that.login) && Objects.equals(accessToken, that.accessToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, accessToken);
+    }
+}
