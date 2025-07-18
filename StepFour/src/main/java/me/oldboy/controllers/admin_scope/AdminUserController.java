43775af4ae480f05828225944a529fd2a639da52
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-18 07:41:42 +0500

    StepFour: update AdminUserController.java

--- StepFour/src/main/java/me/oldboy/controllers/admin_scope/AdminUserController.java
+++ StepFour/src/main/java/me/oldboy/controllers/admin_scope/AdminUserController.java
@@ -2,9 +2,11 @@ package me.oldboy.controllers.admin_scope;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.oldboy.annotations.Auditable;
 import me.oldboy.dto.users.UserReadDto;
 import me.oldboy.dto.users.UserUpdateDeleteDto;
 import me.oldboy.exception.user_exception.UserControllerException;
+import me.oldboy.models.audit.operations.AuditOperationType;
 import me.oldboy.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -24,7 +26,7 @@ public class AdminUserController {
     @Autowired
     private final UserService userService;
 
-    //    @Auditable(operationType = AuditOperationType.DELETE_USER)
+    @Auditable(operationType = AuditOperationType.DELETE_USER)
     @PostMapping("/delete")
     public ResponseEntity<?> deleteUser(@Validated
                                         @RequestBody
@@ -50,7 +52,7 @@ public class AdminUserController {
         }
     }
 
-    //    @Auditable(operationType = AuditOperationType.UPDATE_USER)
+    @Auditable(operationType = AuditOperationType.UPDATE_USER)
     @PostMapping("/update")
     public boolean updateUser(@Validated
                               @RequestBody
