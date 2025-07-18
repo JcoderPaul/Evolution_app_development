3a6d03a67c382280c07ef09498a80c70aa9ce5cb
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-18 07:38:37 +0500

    StepFour: add Auditable.java

---
+++ StepFour/src/main/java/me/oldboy/annotations/Auditable.java
@@ -0,0 +1,24 @@
+package me.oldboy.annotations;
+
+import me.oldboy.models.audit.operations.AuditOperationType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to mark methods for auditing
+ * Аннотация помечающая отслеживаемые методы
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface Auditable {
+
+    /**
+     * Specifies the action type for auditing.
+     *
+     * @return ActionType enum value representing the action type.
+     */
+    AuditOperationType operationType();
+}
