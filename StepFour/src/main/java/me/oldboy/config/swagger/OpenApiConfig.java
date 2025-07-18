45cb45a3ab0326e16685f0ad79e2004b61a9e80f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-18 07:32:49 +0500

    StepFour: add OpenApiConfig.java

---
+++ StepFour/src/main/java/me/oldboy/config/swagger/OpenApiConfig.java
@@ -0,0 +1,39 @@
+package me.oldboy.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+@EnableWebMvc
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "me.oldboy")
+public class OpenApiConfig implements WebMvcConfigurer {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Coworking Simulation")
+                        .contact(new Contact()
+                                .name(": Ермолаев Павел Николаевич")
+                                .email("Jcoder.Paul@gmail.com")));
+    }
+}
