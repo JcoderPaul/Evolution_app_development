c3669c6de2546a4f4afbac8d7fa24c9e162880ea
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-19 06:31:21 +0500

    StepFour: add TestContainerInit.java

---
+++ StepFour/src/test/java/me/oldboy/config/test_data_source/TestContainerInit.java
@@ -0,0 +1,23 @@
+package me.oldboy.config.test_data_source;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public abstract class TestContainerInit {
+    public static final PostgreSQLContainer<?> container =
+            new PostgreSQLContainer<>("postgres:13")
+                    .withInitScript("db/changelog/db.changelog-0.5.sql");
+
+
+    @BeforeAll
+    static void runContainer() {
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("datasource.url", () -> container.getJdbcUrl());
+    }
+}
