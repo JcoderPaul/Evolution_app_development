35cbfac3646b9ef5cc55dc63f3622669048b4994
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-20 06:01:41 +0500

    StepFour: add Slot.java

---
+++ StepFour/src/main/java/me/oldboy/models/entity/Slot.java
@@ -0,0 +1,38 @@
+package me.oldboy.models.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "slotReservation")
+@EqualsAndHashCode(exclude = "slotReservation")
+@Builder
+@Entity
+@Table(name = "slots", schema = "coworking")
+public class Slot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "slot_id")
+    private Long slotId;
+
+    @Column(name = "slot_number")
+    private Integer slotNumber;
+
+    @Column(name = "time_start")
+    private LocalTime timeStart;
+
+    @Column(name = "time_finish")
+    private LocalTime timeFinish;
+
+    /* Связка с сущностью Reservation. Каскадная чистка таблицы all_reservation в случае удаления связного slot-a */
+    @Builder.Default
+    @OneToMany(mappedBy = "slot", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Reservation> slotReservation = new ArrayList<>();
+}
