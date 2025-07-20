84a1e035a911d8e824e735540837adb938d16f27
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-20 06:01:31 +0500

    StepFour: add Reservation.java

---
+++ StepFour/src/main/java/me/oldboy/models/entity/Reservation.java
@@ -0,0 +1,36 @@
+package me.oldboy.models.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "all_reserves", schema = "coworking")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reserve_id")
+    private Long reservationId;
+
+    @Column(name = "reserve_date")
+    private LocalDate reservationDate;
+
+    @ManyToOne(targetEntity = User.class)
+    @JoinColumn(name="user_id", nullable=false) // Ссылка на связанную сущность
+    private User user;
+
+    @ManyToOne(targetEntity = Place.class)
+    @JoinColumn(name="place_id", nullable=false) // Ссылка на связанную сущность
+    private Place place;
+
+    @ManyToOne(targetEntity = Slot.class)
+    @JoinColumn(name="slot_id", nullable=false) // Ссылка на связанную сущность
+    private Slot slot;
+}
