9ba547e8a01d22721e9c82dc87eab6a528d582a2
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-21 06:28:30 +0500

    StepFour: add ReservationServiceException.java

---
+++ StepFour/src/main/java/me/oldboy/exception/reservation_exception/ReservationServiceException.java
@@ -0,0 +1,7 @@
+package me.oldboy.exception.reservation_exception;
+
+public class ReservationServiceException extends RuntimeException {
+    public ReservationServiceException(String msg) {
+        super(msg);
+    }
+}
