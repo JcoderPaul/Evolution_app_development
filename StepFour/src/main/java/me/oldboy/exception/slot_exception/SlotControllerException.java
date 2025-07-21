663da19a9e5138f81e021c88514180f011e76fb9
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-21 06:28:41 +0500

    StepFour: add SlotControllerException.java

---
+++ StepFour/src/main/java/me/oldboy/exception/slot_exception/SlotControllerException.java
@@ -0,0 +1,7 @@
+package me.oldboy.exception.slot_exception;
+
+public class SlotControllerException extends RuntimeException {
+    public SlotControllerException(String msg) {
+        super(msg);
+    }
+}
