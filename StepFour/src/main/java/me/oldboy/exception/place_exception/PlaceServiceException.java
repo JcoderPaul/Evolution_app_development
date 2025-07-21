2b4a1f07a18ddb62fd0d393f167e0c5b0c6aae53
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-21 06:28:06 +0500

    StepFour: add PlaceServiceException.java

---
+++ StepFour/src/main/java/me/oldboy/exception/place_exception/PlaceServiceException.java
@@ -0,0 +1,7 @@
+package me.oldboy.exception.place_exception;
+
+public class PlaceServiceException extends RuntimeException {
+    public PlaceServiceException(String msg) {
+        super(msg);
+    }
+}
