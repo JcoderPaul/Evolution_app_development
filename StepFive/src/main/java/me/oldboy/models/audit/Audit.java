8e8b3818010fb004b0fc0fe7e893d08a5b69d322
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-22 08:24:53 +0500

    StepFive: add Audit.java

---
+++ StepFive/src/main/java/me/oldboy/models/audit/Audit.java
@@ -0,0 +1,43 @@
+package me.oldboy.models.audit;
+
+import jakarta.persistence.*;
+import lombok.*;
+import me.oldboy.models.audit.operations.AuditOperationResult;
+import me.oldboy.models.audit.operations.AuditOperationType;
+
+import java.time.LocalDateTime;
+
+/**
+ * Represents an audit log entry capturing information about a user's actions
+ * Сущность для фиксации в БД CRUD операций пользователя
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "audit_cw", schema = "coworking")
+public class Audit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "aud_id")
+    private Long auditId;
+
+    @Column(name = "creator")
+    private String userName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "aud_result")
+    private AuditOperationResult auditResult;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "aud_operation")
+    private AuditOperationType operationType;
+
+    @Column(name = "aud_entity")
+    private String auditableRecord;
+
+    @Column(name = "aud_time")
+    private LocalDateTime auditTimeStamp;
+}
