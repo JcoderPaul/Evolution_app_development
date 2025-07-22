d8a55b616b3cef5d6e5769e676c5fbefe9caa414
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-23 02:38:40 +0500

    StepFive: add ClientDetailsService.java

---
+++ StepFive/src/main/java/me/oldboy/config/security_details/ClientDetailsService.java
@@ -0,0 +1,32 @@
+package me.oldboy.config.security_details;
+
+import lombok.RequiredArgsConstructor;
+import me.oldboy.exception.user_exception.LoginNotFoundException;
+import me.oldboy.models.entity.User;
+import me.oldboy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClientDetailsService implements UserDetailsService {
+
+	@Autowired
+	private final UserRepository userRepository;
+
+	/* Извлекаем из БД клиентов (user-ов) по имени */
+	@Override
+	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+		Optional<User> mayBeUser = userRepository.findByLogin(login);
+		if (mayBeUser.isEmpty()) {
+			throw new LoginNotFoundException("User : " + login + " not found!");
+		}
+		return new SecurityUserDetails(mayBeUser.get());
+	}
+}
