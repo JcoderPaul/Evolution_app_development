7144664c5b2ba863c59b184bd725beabe5e65efd
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-24 06:18:38 +0500

    StepFive: add AdminUserController.java

---
+++ StepFive/src/main/java/me/oldboy/controllers/admin_scope/AdminUserController.java
@@ -0,0 +1,62 @@
+package me.oldboy.controllers.admin_scope;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.oldboy.dto.users.UserReadDto;
+import me.oldboy.dto.users.UserUpdateDeleteDto;
+import me.oldboy.exception.user_exception.UserControllerException;
+import me.oldboy.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@NoArgsConstructor
+@RequestMapping("/api/admin/users")
+public class AdminUserController {
+
+    @Autowired
+    private UserService userService;
+
+    //    @Auditable(operationType = AuditOperationType.DELETE_USER)
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteUser(@Validated
+                                        @RequestBody
+                                        UserUpdateDeleteDto deleteDto) {
+
+        Optional<UserReadDto> userForRemove = userService.findByLogin(deleteDto.login());
+
+        if (userForRemove.isEmpty()) {
+            throw new UserControllerException("Пользователь для удаления не найден!");
+        }
+        if (userForRemove.get().userId() != deleteDto.userId()) {
+            throw new UserControllerException("Вы уверенны что хотите удалить именно этого пользователя?");
+        }
+
+        if (userService.delete(userForRemove.get().userId())) {
+            return ResponseEntity.ok().body("{\"message\": \"User removed!\"}");
+        } else {
+            return ResponseEntity.badRequest().body("{\"message\": \"Remove failed!\"}");
+        }
+    }
+
+    //    @Auditable(operationType = AuditOperationType.UPDATE_USER)
+    @PostMapping("/update")
+    public boolean updateUser(@Validated
+                              @RequestBody
+                              UserUpdateDeleteDto updateDto) {
+        return userService.update(updateDto);
+    }
+
+    @GetMapping("/all")
+    public List<UserReadDto> getAllUser() {
+        return userService.findAll();
+    }
+}
