003d1c5a53e1280ce0fee74ff43baf61fcd52551
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-25 07:54:12 +0500

    StepFive: add UserUpdateDeleteDto.java

---
+++ StepFive/src/main/java/me/oldboy/dto/users/UserUpdateDeleteDto.java
@@ -0,0 +1,24 @@
+package me.oldboy.dto.users;
+
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+
+/* Никакой кириллицы в message */
+@Builder
+public record UserUpdateDeleteDto(@NotNull
+                                  @Positive(message = "User ID must be positive")
+                                  Long userId,
+
+                                  @NotEmpty
+                                  @Size(min = 3, max = 64, message = "Wrong format (to short/to long)")
+                                  String login,
+
+                                  @NotEmpty
+                                  @Size(min = 3, max = 64, message = "Wrong format (to short/to long)")
+                                  String password,
+
+                                  @NotNull(message = "The user role can not be NULL")
+                                  @NotEmpty(message = "The user role can not be EMPTY")
+                                  @Pattern(regexp = "ADMIN|USER", message = "The user role can only be (ADMIN or USER)")
+                                  String role) {
+}
