7ec463acc01a480517ff6b677ee28df5e536860d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-25 07:53:51 +0500

    StepFive: add UserCreateDto.java

---
+++ StepFive/src/main/java/me/oldboy/dto/users/UserCreateDto.java
@@ -0,0 +1,19 @@
+package me.oldboy.dto.users;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+@Builder
+public record UserCreateDto(@NotEmpty(message = "The login can not be EMPTY")
+                            @Size(min = 3, max = 64, message = "Wrong format (to short/to long)")
+                            String login,
+                            @NotEmpty (message = "Password can not be EMPTY")
+                            @Size(min = 3, max = 64, message = "Wrong format (to short/to long)")
+                            String password,
+                            @NotEmpty (message = "The user role can not be EMPTY")
+                            @Pattern(regexp = "ADMIN|USER", message = "The user role can only be (ADMIN or USER)")
+                            String role) {
+}
