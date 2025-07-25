1a4b682895f091b63549911c36c05e7795cb7dde
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-25 07:53:29 +0500

    StepFive: add SlotCreateDeleteDto.java

---
+++ StepFive/src/main/java/me/oldboy/dto/slots/SlotCreateDeleteDto.java
@@ -0,0 +1,21 @@
+package me.oldboy.dto.slots;
+
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Builder;
+
+import java.time.LocalTime;
+
+@Builder
+public record SlotCreateDeleteDto(@NotNull
+                                  @PositiveOrZero(message = "Slot number can not be blank/null/negative, it must be greater than or equal to 0")
+                                  Integer slotNumber,
+                                  @NotNull(message = "Start time can not be null")
+                                  @Temporal(TemporalType.TIME)
+                                  LocalTime timeStart,
+                                  @NotNull(message = "Finish time can not be null")
+                                  @Temporal(TemporalType.TIME)
+                                  LocalTime timeFinish) {
+}
