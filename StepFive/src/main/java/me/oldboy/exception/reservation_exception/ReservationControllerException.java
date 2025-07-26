b60a8da31012077104e904f4a9ef3c81911e48d2
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-26 05:31:53 +0500

    StepFive: add ReservationControllerException.java

---
+++ StepFive/src/main/java/me/oldboy/exception/reservation_exception/ReservationControllerException.java
@@ -0,0 +1,7 @@
+package me.oldboy.exception.reservation_exception;
+
+public class ReservationControllerException extends RuntimeException {
+    public ReservationControllerException(String msg) {
+        super(msg);
+    }
+}
