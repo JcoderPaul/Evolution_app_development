802c6706f30299b331422ad9aea1432b92782127
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-26 05:58:59 +0500

    StepFive: update UserUpdateDeleteDto.java

--- StepFive/src/main/java/me/oldboy/dto/users/UserUpdateDeleteDto.java
+++ StepFive/src/main/java/me/oldboy/dto/users/UserUpdateDeleteDto.java
@@ -17,7 +17,6 @@ public record UserUpdateDeleteDto(@NotNull
                                   @Size(min = 3, max = 64, message = "Wrong format (to short/to long)")
                                   String password,
 
-                                  @NotNull(message = "The user role can not be NULL")
                                   @NotEmpty(message = "The user role can not be EMPTY")
                                   @Pattern(regexp = "ADMIN|USER", message = "The user role can only be (ADMIN or USER)")
                                   String role) {
