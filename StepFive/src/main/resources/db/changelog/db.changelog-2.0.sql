a4244e3ce7d7cefdb6f90c3c192f4e64ae35469f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-27 06:41:54 +0500

    StepFive: add db.changelog-2.0.sql

---
+++ StepFive/src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset oldboy:1
+CREATE TABLE IF NOT EXISTS coworking.slots
+(
+    slot_id BIGSERIAL PRIMARY KEY ,
+    slot_number INTEGER NOT NULL UNIQUE ,
+    time_start TIME NOT NULL UNIQUE,
+	time_finish TIME NOT NULL UNIQUE CONSTRAINT right_time CHECK (time_finish > time_start),
+    UNIQUE (slot_number, time_start, time_finish)
+);
+--rollback drop table coworking.slots
