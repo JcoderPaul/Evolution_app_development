2a7b0eb5c7651734e2a4b0b1a34811e6ebf2f92b
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-28 05:49:06 +0500

    StepFive: add PlaceMapper.java

---
+++ StepFive/src/main/java/me/oldboy/mapper/PlaceMapper.java
@@ -0,0 +1,44 @@
+package me.oldboy.mapper;
+
+/*
+Теоретически, сущность Place не несет какой-то закрытой информации,
+например password, как сущность User, и которую мы бы не хотели
+отдавать во вне. Однако, в качестве демонстрации работы MapStruct
+мы создадим для каждой из наших 4-х основных сущностей по два DTO
+класса (EntityReadDto и EntityCreateDto)
+*/
+
+import me.oldboy.dto.places.PlaceCreateDeleteDto;
+import me.oldboy.dto.places.PlaceReadUpdateDto;
+import me.oldboy.models.entity.Place;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * Mapper interface for converting Place entities to PlaceReadDto and vice versa.
+ * Интерфейс для преобразования сущности Place в PlaceReadDto / PlaceCreateDto в Place
+ */
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PlaceMapper {
+
+    PlaceMapper INSTANCE = Mappers.getMapper(PlaceMapper.class);
+
+    /**
+     * Converts a Place entity to a PlaceReadDto.
+     * Преобразование сущности Place в PlaceReadDto.
+     *
+     * @param place the Place entity to convert
+     * @return the converted PlaceReadDto
+     */
+    PlaceReadUpdateDto mapToPlaceReadDto(Place place);
+
+    /**
+     * Converts a PlaceCreateDto to a Place entity.
+     * Преобразование PlaceCreateDto в сущность Place.
+     *
+     * @param placeCreateDto the DTOs to convert
+     * @return the converted Place entity
+     */
+    Place mapToEntity(PlaceCreateDeleteDto placeCreateDto);
+}
