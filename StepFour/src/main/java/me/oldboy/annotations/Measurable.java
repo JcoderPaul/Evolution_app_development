43f5777a4ef23ff062124cc15631e428ec43c0f8
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-04 06:22:27 +0500

    StepFour: update Measurable.java

--- StepFour/src/main/java/me/oldboy/annotations/Measurable.java
+++ StepFour/src/main/java/me/oldboy/annotations/Measurable.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The annotation applies to methods that require execution time measurement.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface Measurable {
