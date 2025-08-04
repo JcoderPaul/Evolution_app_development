1772d60c32f1f9d407dbcb22c2a00ff4ced6a6fd
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-04 06:22:56 +0500

    StepFour: update MethodSpeedCalcAspect.java

--- StepFour/src/main/java/me/oldboy/aspects/MethodSpeedCalcAspect.java
+++ StepFour/src/main/java/me/oldboy/aspects/MethodSpeedCalcAspect.java
@@ -7,6 +7,14 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+/**
+ * The aspect is used to measure the execution
+ * time of a method annotated with @Measurable
+ *
+ * It records the method name, the start of the
+ * method, the end of the method, and the execution t
+ * ime.
+ */
 @Slf4j
 @Aspect
 @Component
@@ -15,6 +23,14 @@ public class MethodSpeedCalcAspect {
     @Pointcut("@annotation(me.oldboy.annotations.Measurable) && execution(* *(..))")
     public void speedMeasurable(){}
 
+    /**
+     * Advice that wraps around the methods matched by the pointcut.
+     * It logs the start and end of the method execution, as well as the execution time.
+     *
+     * @param joinPoint the join point
+     * @return the result of the method execution
+     * @throws Throwable if the invoked proceed throws anything
+     */
     @Around("speedMeasurable()")
     public Object loggableMethods(ProceedingJoinPoint joinPoint) throws Throwable {
         String methodName = joinPoint.getSignature().toShortString();
