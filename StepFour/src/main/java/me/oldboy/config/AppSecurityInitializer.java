df7c1154bc9ddfa6031f92f0c3d7e084f1e56532
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-06 05:46:06 +0500

    StepFour: update AppSecurityInitializer.java

--- StepFour/src/main/java/me/oldboy/config/AppSecurityInitializer.java
+++ StepFour/src/main/java/me/oldboy/config/AppSecurityInitializer.java
@@ -2,5 +2,9 @@ package me.oldboy.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+/**
+ * Registers the DelegatingFilterProxy to use the springSecurityFilterChain
+ * before any other registered Filter.
+ */
 public class AppSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 }
