de7b60827553944a74562616abacd3f5cc065534
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-08 06:00:50 +0500

    StepFour: update JwtAuthRequest.java

--- StepFour/src/main/java/me/oldboy/dto/jwt/JwtAuthRequest.java
+++ StepFour/src/main/java/me/oldboy/dto/jwt/JwtAuthRequest.java
@@ -7,16 +7,25 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Data Transfer Object (DTO) representing authentication information.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class JwtAuthRequest {
 
+    /**
+     * Auth user login
+     */
     @NotEmpty
     @Size(min = 3, max = 64, message = "Wrong format (to short/to long)")
     private String login;
 
+    /**
+     * Auth user password
+     */
     @NotEmpty
     @Size(min = 3, max = 128, message = "Wrong format (to short/to long)")
     private String password;
