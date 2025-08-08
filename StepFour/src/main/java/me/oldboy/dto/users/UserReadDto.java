dee14d927875bddf5f32405082f0675e67dc6eb6
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-08 06:03:30 +0500

    StepFour: update UserReadDto.java

--- StepFour/src/main/java/me/oldboy/dto/users/UserReadDto.java
+++ StepFour/src/main/java/me/oldboy/dto/users/UserReadDto.java
@@ -2,6 +2,9 @@ package me.oldboy.dto.users;
 
 import me.oldboy.models.entity.options.Role;
 
+/**
+ * A record representing user read info.
+ */
 public record UserReadDto (Long userId,
                            String login,
                            Role role){
