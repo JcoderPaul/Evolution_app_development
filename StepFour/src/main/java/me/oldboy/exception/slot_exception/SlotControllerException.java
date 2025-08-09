43c9a352d0b8d4ad5c784c50eefeb2a01c4f32cd
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-09 06:09:30 +0500

    StepFour: update SlotControllerException.java

---
+++ StepFour/src/main/java/me/oldboy/exception/slot_exception/SlotControllerException.java
@@ -0,0 +1,20 @@
+package me.oldboy.exception.slot_exception;
+
+/**
+ * Exception thrown in the controller layer when exceptional
+ * situations occur when accessing the slot entity: slot not
+ * found, slot does not exist, etc.
+ *
+ * This exception extends {@link RuntimeException}, indicating
+ * that the requested user was not found.
+ */
+public class SlotControllerException extends RuntimeException {
+
+    /**
+     * Creates a {@code SlotControllerException} with a message describing the exception.
+     * @param msg — a detailed message.
+     */
+    public SlotControllerException(String msg) {
+        super(msg);
+    }
+}
