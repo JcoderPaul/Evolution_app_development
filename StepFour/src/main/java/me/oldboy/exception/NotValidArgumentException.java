309cc0563acd1e7bb2186575fc605e597d430124
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-09 06:10:24 +0500

    StepFour: update NotValidArgumentException.java

--- StepFour/src/main/java/me/oldboy/exception/NotValidArgumentException.java
+++ StepFour/src/main/java/me/oldboy/exception/NotValidArgumentException.java
@@ -1,6 +1,17 @@
 package me.oldboy.exception;
 
+/**
+ * Exception raised when an invalid parameter is entered when creating a reserve.
+ *
+ * This exception extends {@link RuntimeException} to indicate that the entered
+ * parameter is invalid, or a combination of both.
+ */
 public class NotValidArgumentException extends RuntimeException {
+
+    /**
+     * Creates a {@code NotValidArgumentException} with a description of the exception.
+     * @param msg — a detailed message.
+     */
     public NotValidArgumentException(String msg) {
         super(msg);
     }
