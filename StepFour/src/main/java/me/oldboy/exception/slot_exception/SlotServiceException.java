cd74f46007dc8867442381fa4c86da0495a97b41
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-09 06:08:54 +0500

    StepFour: update SlotServiceException.java

--- StepFour/src/main/java/me/oldboy/exception/slot_exception/SlotServiceException.java
+++
@@ -1,7 +0,0 @@
-package me.oldboy.exception.slot_exception;
-
-public class SlotServiceException extends RuntimeException {
-    public SlotServiceException(String msg) {
-        super(msg);
-    }
-}
