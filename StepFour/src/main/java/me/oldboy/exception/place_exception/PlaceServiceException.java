1793bd54db1203ccdbbf5e2400822e645a7f204b
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-09 06:08:09 +0500

    StepFour: update PlaceServiceException.java

--- StepFour/src/main/java/me/oldboy/exception/place_exception/PlaceServiceException.java
+++ StepFour/src/main/java/me/oldboy/exception/place_exception/PlaceServiceException.java
@@ -1,6 +1,19 @@
 package me.oldboy.exception.place_exception;
 
+/**
+ * An exception is thrown in the service layer if incorrect actions
+ * are performed when working with the place entity, for example: place
+ * with id not found, place with number not found, incorrect time range, etc.
+ *
+ * This exception extends {@link RuntimeException}, indicating that an
+ * error occurred in the service layer when working with the place entity.
+ */
 public class PlaceServiceException extends RuntimeException {
+
+    /**
+     * Creates a {@code PlaceServiceException} with a message describing the exception.
+     * @param msg — a detailed message.
+     */
     public PlaceServiceException(String msg) {
         super(msg);
     }
