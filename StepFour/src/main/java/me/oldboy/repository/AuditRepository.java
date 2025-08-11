5af731769e4f09c7718b3cb5556bc578a1337614
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-11 05:30:54 +0500

    StepFour: update AuditRepository.java with JDoc

--- StepFour/src/main/java/me/oldboy/repository/AuditRepository.java
+++ StepFour/src/main/java/me/oldboy/repository/AuditRepository.java
@@ -5,6 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Data Access Object (DAO) interface for managing Audit entities.
+ *
+ * Extends the generic JpaRepository interface with special operations
+ * for working with Audit entities.
+ */
 @Repository
 public interface AuditRepository extends JpaRepository<Audit, Long>, CrudRepository<Audit, Long> {
 
