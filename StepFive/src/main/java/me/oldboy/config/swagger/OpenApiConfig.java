7ca3b614ac3db89bf2f2fa1d11b91f68e80bd227
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-12 07:04:28 +0500

    StepFive: update OpenApiConfig.java

--- StepFive/src/main/java/me/oldboy/config/swagger/OpenApiConfig.java
+++ StepFive/src/main/java/me/oldboy/config/swagger/OpenApiConfig.java
@@ -14,6 +14,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Class configuration Swagger
+ */
 @Slf4j
 @Configuration
 @EnableWebMvc
