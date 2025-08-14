2cc09b0860262b2046949ec0282eda0d6f9e601f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-14 06:13:13 +0500

    StepFive: update UserReadDto.java

--- StepFive/src/main/java/me/oldboy/dto/users/UserReadDto.java
+++ StepFive/src/main/java/me/oldboy/dto/users/UserReadDto.java
@@ -3,6 +3,9 @@ package me.oldboy.dto.users;
 import lombok.Builder;
 import me.oldboy.models.entity.options.Role;
 
+/**
+ * A record representing user read info.
+ */
 @Builder
 public record UserReadDto (Long userId,
                            String login,
