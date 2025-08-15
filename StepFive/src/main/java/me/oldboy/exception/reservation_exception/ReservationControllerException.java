90a417abd117c02a727d43fc21ad7fb758babd6c
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-15 05:01:28 +0500

    StepFive: update ReservationControllerException.java

--- StepFive/src/main/java/me/oldboy/exception/reservation_exception/ReservationControllerException.java
+++ StepFive/src/main/java/me/oldboy/exception/reservation_exception/ReservationControllerException.java
@@ -1,6 +1,18 @@
 package me.oldboy.exception.reservation_exception;
 
+/**
+ * Exception thrown by controller layer in various situations, such as:
+ * reservation does not exist or was not found, reservation is not possible,
+ * duplicate reservation, insufficient rights to make a reservation, etc.
+ *
+ * This exception extends {@link RuntimeException}, indicating that an error
+ * occurred in the reservation thread controller layer.
+ */
 public class ReservationControllerException extends RuntimeException {
+    /**
+     * Creates a {@code ReservationServiceException} with a message describing the exception.
+     * @param msg — a detailed message.
+     */
     public ReservationControllerException(String msg) {
         super(msg);
     }
