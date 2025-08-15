11ccaaf5bc94d637eaf08ea54936152c7b79405a
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-15 05:02:27 +0500

    StepFive: update UserControllerException.java

--- StepFive/src/main/java/me/oldboy/exception/user_exception/UserControllerException.java
+++ StepFive/src/main/java/me/oldboy/exception/user_exception/UserControllerException.java
@@ -1,6 +1,18 @@
 package me.oldboy.exception.user_exception;
 
+/**
+ * Exception thrown from the controller layer, in the thread handling
+ * any requests for the user entity: user with the name not found,
+ * password not correct, for example.
+ *
+ * This exception extends {@link RuntimeException}, indicating exceptions
+ * in the controller layer, after any requests for the user entity.
+ */
 public class UserControllerException extends RuntimeException {
+    /**
+     * Creates a {@code UserControllerException} with a description of the exception.
+     * @param msg — detailed message.
+     */
     public UserControllerException(String msg){
         super(msg);
     }
