a59aa8f8c1abd11b1793ce416c4be03e81bebe41
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-15 05:02:15 +0500

    StepFive: update LoginNotFoundException.java

--- StepFive/src/main/java/me/oldboy/exception/user_exception/LoginNotFoundException.java
+++ StepFive/src/main/java/me/oldboy/exception/user_exception/LoginNotFoundException.java
@@ -1,6 +1,16 @@
 package me.oldboy.exception.user_exception;
 
+/**
+ * Exception raised in the service layer if the user entered an invalid password.
+ *
+ * This exception extends {@link RuntimeException} to indicate that the user entered
+ * an invalid password.
+ */
 public class LoginNotFoundException extends RuntimeException {
+    /**
+     * Creates a {@code LoginNotFoundException} with a description of the exception.
+     * @param msg — detailed message.
+     */
     public LoginNotFoundException(String msg) {
         super(msg);
     }
