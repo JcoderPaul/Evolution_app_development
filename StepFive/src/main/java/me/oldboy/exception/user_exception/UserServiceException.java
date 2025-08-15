15e0fd0c03395bc17df4e1442227a0e1e8fcd183
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-15 05:02:39 +0500

    StepFive: update UserServiceException.java

--- StepFive/src/main/java/me/oldboy/exception/user_exception/UserServiceException.java
+++ StepFive/src/main/java/me/oldboy/exception/user_exception/UserServiceException.java
@@ -1,6 +1,18 @@
 package me.oldboy.exception.user_exception;
 
+/**
+ * Exception thrown from the service layer, in the thread handling
+ * any requests for the user entity: user not found, e.g.
+ *
+ * This exception extends {@link RuntimeException}, indicating
+ * exceptions in the service layer, e.g. user with the requested
+ * id not found.
+ */
 public class UserServiceException extends RuntimeException {
+    /**
+     * Creates a {@code UserServiceException} with a description of the exception.
+     * @param msg — a detailed message.
+     */
     public UserServiceException(String msg){
         super(msg);
     }
