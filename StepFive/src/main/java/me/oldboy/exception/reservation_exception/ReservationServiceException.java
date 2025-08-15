603bdc6c22b0d30020a20dd3c8f7fefa41d3e97a
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-15 05:01:40 +0500

    StepFive: update ReservationServiceException.java

--- StepFive/src/main/java/me/oldboy/exception/reservation_exception/ReservationServiceException.java
+++ StepFive/src/main/java/me/oldboy/exception/reservation_exception/ReservationServiceException.java
@@ -1,6 +1,19 @@
 package me.oldboy.exception.reservation_exception;
 
+/**
+ * Exception thrown from the service layer of the reservation
+ * sequence in case of, for example: reservation not possible,
+ * reservation not found, update not possible, etc.
+ *
+ * This exception extends {@link RuntimeException}, indicating
+ * that an exception occurred in the service layer while working
+ * with the reservation entity.
+ */
 public class ReservationServiceException extends RuntimeException {
+    /**
+     * Creates a {@code ReservationServiceException} with a message describing the exception.
+     * @param msg — a detailed message.
+     */
     public ReservationServiceException(String msg) {
         super(msg);
     }
