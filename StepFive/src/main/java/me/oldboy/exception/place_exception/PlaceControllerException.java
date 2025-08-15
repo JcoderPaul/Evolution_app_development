ccf153e38fe926797d84ad3c0456aeed9a94971d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-15 05:01:00 +0500

    StepFive: update PlaceControllerException.java

--- StepFive/src/main/java/me/oldboy/exception/place_exception/PlaceControllerException.java
+++ StepFive/src/main/java/me/oldboy/exception/place_exception/PlaceControllerException.java
@@ -1,6 +1,18 @@
 package me.oldboy.exception.place_exception;
 
+/**
+ * Exception thrown in the controller layer when exceptional
+ * situations occur when accessing the place entity: place not
+ * found, place does not exist, duplication create, etc.
+ *
+ * This exception extends {@link RuntimeException}, indicating
+ * that the requested user was not found.
+ */
 public class PlaceControllerException extends RuntimeException {
+    /**
+     * Creates a {@code PlaceControllerException} with a message describing the exception.
+     * @param msg — a detailed message.
+     */
     public PlaceControllerException(String msg) {
         super(msg);
     }
