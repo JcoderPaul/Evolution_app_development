6afc1ec6f34b10652605b64f840747a3fac556bd
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-08-16 06:35:55 +0500

    StepFive: update Role.java

--- StepFive/src/main/java/me/oldboy/models/entity/options/Role.java
+++ StepFive/src/main/java/me/oldboy/models/entity/options/Role.java
@@ -1,6 +1,16 @@
 package me.oldboy.models.entity.options;
 
+/**
+ * Enumeration representing different roles for users.
+ */
 public enum Role {
+    /**
+     * Represents an administrator role with elevated privileges to access service resources.
+     */
     ADMIN,
+
+    /**
+     * Represents a regular user role.
+     */
     USER
 }
